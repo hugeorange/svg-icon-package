a53e6207cc60714083449caf789c715f2dce399d
LEIYAO123 <lei@rushable.io>
2024-03-22 16:40:06 +0800

    feat: update readme

--- .dumirc.ts
+++ .dumirc.ts
@@ -2,13 +2,7 @@ import { defineConfig } from 'dumi';
 
 export default defineConfig({
   outputPath: 'docs-dist',
-  base: '/rushable-icon2/',
-  publicPath: '/rushable-icon2/',
+  base: '/svg-icon-package/',
+  publicPath: '/svg-icon-package/',
   exportStatic: {}, // 后续会部署到 github pages 直接全部生成静态页面 不走前端路由
-  themeConfig: {
-    name: '',
-    logo: '/rushable-icon2/logo.svg',
-    nav: [],
-    showLineNum: true,
-  },
 });
